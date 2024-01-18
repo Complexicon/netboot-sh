4b074344bb2f765790c140336f6c5ee3ac76e5a8
Complexicon <contact@cmplx.dev>
2024-01-18 16:29:31 +0100

    Create imgoverlay.sh

---
+++ imgoverlay.sh
@@ -0,0 +1,104 @@
+#!/bin/sh -e
+
+HOSTNAME="$1"
+if [ -z "$HOSTNAME" ]; then
+	echo "usage: $0 hostname"
+	exit 1
+fi
+
+cleanup() {
+	rm -rf "$tmp"
+}
+
+makefile() {
+	OWNER="$1"
+	PERMS="$2"
+	FILENAME="$3"
+	cat > "$FILENAME"
+	chown "$OWNER" "$FILENAME"
+	chmod "$PERMS" "$FILENAME"
+}
+
+rc_add() {
+	mkdir -p "$tmp"/etc/runlevels/"$2"
+	ln -sf /etc/init.d/"$1" "$tmp"/etc/runlevels/"$2"/"$1"
+}
+
+tmp="$(mktemp -d)"
+trap cleanup EXIT
+
+mkdir -p "$tmp"/etc
+makefile root:root 0644 "$tmp"/etc/hostname <<EOF
+$HOSTNAME
+EOF
+
+#mkdir -p "$tmp"/etc/local.d
+#makefile root:root 0644 "$tmp"/etc/local.d/installer.script <<EOF
+#exec > >(tee -a /dev/tty0) 2>&1
+#setup-keymap
+#ash
+#EOF
+
+#mkdir -p "$tmp"/etc/network
+#makefile root:root 0644 "$tmp"/etc/network/interfaces <<EOF
+#auto lo
+#iface lo inet loopback
+
+#auto eth0
+#iface eth0 inet dhcp
+#EOF
+
+mkdir -p "$tmp"/etc/apk
+makefile root:root 0644 "$tmp"/etc/apk/world <<EOF
+alpine-base
+debootstrap
+nano
+curl
+wget
+agetty
+EOF
+
+makefile root:root 0755 "$tmp"/etc/inittab <<EOF
+# /etc/inittab
+
+::sysinit:/sbin/openrc sysinit
+::sysinit:/sbin/openrc boot
+::wait:/sbin/openrc default
+
+tty1::respawn:/sbin/agetty 38400 tty1 --autologin root --noclear
+tty2::respawn:/sbin/getty 38400 tty2
+
+::shutdown:/sbin/openrc shutdown
+
+ttyS0::respawn:/sbin/getty -L 0 ttyS0 vt100
+EOF
+
+makefile root:root 0755 "$tmp"/etc/profile <<EOF
+#!/bin/sh
+clear
+setup-keymap
+setup-interfaces
+rc-update add networking boot
+openrc boot
+sh <(curl -s -L git.cmplx.dev/netboot-sh/raw/main/main.sh)
+EOF
+
+rc_add devfs sysinit
+rc_add dmesg sysinit
+rc_add mdev sysinit
+rc_add hwdrivers sysinit
+rc_add modloop sysinit
+
+rc_add hwclock boot
+rc_add modules boot
+rc_add sysctl boot
+rc_add hostname boot
+rc_add bootmisc boot
+rc_add syslog boot
+rc_add local default
+
+rc_add mount-ro shutdown
+rc_add killprocs shutdown
+rc_add savecache shutdown
+
+tar -c -C "$tmp" etc | gzip -9n > $HOSTNAME.apkovl.tar.gz
