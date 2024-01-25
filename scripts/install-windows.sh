330f790ee6909f2161be36681fa1ff02d32d98db
Complexicon <contact@cmplx.dev>
2024-01-25 22:25:08 +0100

    Update install-windows.sh

--- scripts/install-windows.sh
+++ scripts/install-windows.sh
@@ -7,9 +7,9 @@ https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/co
 
 EOF
 apk update
-apk add syslinux ntfs-3g ntfs-3g-progs xxd libc6-compat util-linux fuse coreutils grep wimlib
+apk add syslinux ntfs-3g ntfs-3g-progs xxd libc6-compat util-linux fuse coreutils grep wimlib git
 modprobe fuse
 git clone --depth=1 https://git.cmplx.dev/netboot-sh
-cd netboot-sh
-chmod +x
+cd netboot-sh/cloud-windows
+chmod +x *
 echo "entering debug shell"
