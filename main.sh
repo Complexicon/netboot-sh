9653a17770da3b06217e5127c76610ba8647d5e7
Complexicon <contact@cmplx.dev>
2024-01-25 22:27:37 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-SCRIPTS=$(curl --silent https://api.github.com/repos/Complexicon/netboot-sh/contents/scripts?ref=main | jq -r ". [] | select(.name|endswith(\"sh\")) | .name")
-echo "Choose which script to run"
-SCRIPT_TO_RUN=$(echo "$SCRIPTS" | gum choose)
-sh <(curl -s -L git.cmplx.dev/netboot-sh/raw/main/scripts/${SCRIPT_TO_RUN})
+
+while true
+do
+  SCRIPTS=$(curl --silent https://api.github.com/repos/Complexicon/netboot-sh/contents/scripts?ref=main | jq -r ". [] | select(.name|endswith(\"sh\")) | .name")
+  echo "Choose which script to run"
+  SCRIPT_TO_RUN=$(echo "$SCRIPTS" | gum choose)
+  echo "Running '$SCRIPT_TO_RUN'..."
+  sh <(curl -s -L git.cmplx.dev/netboot-sh/raw/main/scripts/${SCRIPT_TO_RUN})
+done
