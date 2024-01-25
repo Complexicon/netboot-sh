8fd51fe03a525dbf3c46c4589bb258fe9a696976
Complexicon <contact@cmplx.dev>
2024-01-25 21:00:34 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sh <(curl -s -L git.cmplx.dev/netboot-sh/raw/main/test.sh) #edit this
+SCRIPTS=$(curl https://api.github.com/repos/Complexicon/netboot-sh/contents | jq . [] \| select(.name\|endswith("sh")) \| .name)
+echo $SCRIPTS
+#sh <(curl -s -L git.cmplx.dev/netboot-sh/raw/main/test.sh) #edit this
