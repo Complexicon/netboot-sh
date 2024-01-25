b96a745aa66f61b96d3bdd392feb69b47744f7cf
Complexicon <contact@cmplx.dev>
2024-01-25 20:26:00 +0100

    Update doall.sh

--- aports/doall.sh
+++ aports/doall.sh
@@ -2,4 +2,4 @@
 SUDO="" abuild-keygen -i -a -n
 #cp /src/aports/scripts/genapkovl-mkimgoverlay.sh /github/home/.mkimage/overlay.sh
 chmod +x /github/home/.mkimage/overlay.sh
-/src/aports/scripts/mkimage.sh --outdir /github/workspace --arch x86_64 --profile installeriso --repository https://dl-cdn.alpinelinux.org/alpine/latest-stable/main/
+/src/aports/scripts/mkimage.sh --outdir /github/workspace --arch x86_64 --profile installeriso --repository https://dl-cdn.alpinelinux.org/alpine/latest-stable/main/ --repository https://dl-cdn.alpinelinux.org/alpine/latest-stable/community/
