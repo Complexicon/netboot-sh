fbd88ec273889f471386078fe9ce80a78efc02a9
Complexicon <contact@cmplx.dev>
2024-01-25 22:23:01 +0100

    Create install-windows.sh

---
+++ scripts/install-windows.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "fetching dependencies"
+cat > /etc/apk/repositories << EOF; $(echo)
+
+https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/main/
+https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/community/
+
+EOF
+apk update
+apk add syslinux ntfs-3g ntfs-3g-progs xxd libc6-compat util-linux fuse coreutils grep wimlib
+modprobe fuse
+git clone --depth=1 https://git.cmplx.dev/netboot-sh
+cd netboot-sh
+chmod +x
+echo "entering debug shell"
