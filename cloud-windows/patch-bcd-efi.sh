82412e1549ad88f9021a301a4a27def974893e18
Complexicon <contact@cmplx.dev>
2024-01-26 16:13:53 +0100

    Update patch-bcd-efi.sh

--- cloud-windows/patch-bcd-efi.sh
+++ cloud-windows/patch-bcd-efi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-TO_PATCH="\x00\x00\x50\x1F\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x16\x13\x2B\x32\x00\x00\x00\x00"
-MATCHES=$(LANG=C grep --only-matching --byte-offset --binary --text --perl-regexp "$TO_PATCH" BCD_reference_bios | cut -d: -f1)
+TO_PATCH="\xC1\x6E\x42\x4A\x9D\xC8\xD3\x48\x8E\xA0\x7A\x85\x9A\x82\x6C\x85\x00\x00\x00\x00\x00\x00\x00\x00\x8F\x62\x5E\x6A\x8C\x8B\x03\x42\x87\x49\x8D\x34\xE3\x8E\xCC\xC0"
+MATCHES=$(LANG=C grep --only-matching --byte-offset --binary --text --perl-regexp "$TO_PATCH" BCD_reference_efi | cut -d: -f1)
 
 DISK_UUID=$(sfdisk --disk-id $1)
 PART_UUID=$(sfdisk --part-uuid $1 $2)
@@ -10,9 +10,11 @@ printf "{disk=%s,part=%s}\n" $DISK_UUID $PART_UUID
 ./uuid2bin.sh "$DISK_UUID" DISK.bin
 ./uuid2bin.sh "$PART_UUID" PART.bin
 
+cp BCD_reference_efi BCD_patched_efi
+
 for x in $MATCHES
 do
     echo "patching at [$x]"
-    dd if=PART.bin of=BCD_patched_bios bs=1 seek=$x conv=notrunc
-    dd if=DISK.bin of=BCD_patched_bios bs=1 seek=$(($x + 24)) conv=notrunc
+    dd if=PART.bin of=BCD_patched_efi bs=1 seek=$x conv=notrunc status=none
+    dd if=DISK.bin of=BCD_patched_efi bs=1 seek=$(($x + 24)) conv=notrunc status=none
 done
